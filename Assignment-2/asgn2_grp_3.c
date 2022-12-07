85153803f613d27a32e618899f724ae3e35097ad
Ashutosh Kumar Singh <60400441+ashutoshaks@users.noreply.github.com>
2022-12-07 15:25:18 +0530

    Update asgn2_grp_3.c

--- Assignment-2/asgn2_grp_3.c
+++ Assignment-2/asgn2_grp_3.c
@@ -24,9 +24,9 @@ MODULE_VERSION("0.1");
 #define PB2_SET_CAPACITY _IOW(0x10, 0x31, int32_t *)
 #define PB2_INSERT_INT _IOW(0x10, 0x32, int32_t *)
 #define PB2_INSERT_PRIO _IOW(0x10, 0x33, int32_t *)
-#define PB2_GET_INFO _IOW(0x10, 0x34, int32_t *)
-#define PB2_GET_MIN _IOW(0x10, 0x35, int32_t *)
-#define PB2_GET_MAX _IOW(0x10, 0x36, int32_t *)
+#define PB2_GET_INFO _IOR(0x10, 0x34, int32_t *)
+#define PB2_GET_MIN _IOR(0x10, 0x35, int32_t *)
+#define PB2_GET_MAX _IOR(0x10, 0x36, int32_t *)
 
 struct element {
     int val;
