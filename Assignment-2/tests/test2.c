4845c4021c66c7131c16d2d6ee65583ea95d8976
Ashutosh Kumar Singh <60400441+ashutoshaks@users.noreply.github.com>
2022-12-07 15:26:07 +0530

    Update test2.c

--- Assignment-2/tests/test2.c
+++ Assignment-2/tests/test2.c
@@ -14,9 +14,9 @@
 #define PB2_SET_CAPACITY _IOW(0x10, 0x31, int32_t *)
 #define PB2_INSERT_INT _IOW(0x10, 0x32, int32_t *)
 #define PB2_INSERT_PRIO _IOW(0x10, 0x33, int32_t *)
-#define PB2_GET_INFO _IOW(0x10, 0x34, int32_t *)
-#define PB2_GET_MIN _IOW(0x10, 0x35, int32_t *)
-#define PB2_GET_MAX _IOW(0x10, 0x36, int32_t *)
+#define PB2_GET_INFO _IOR(0x10, 0x34, int32_t *)
+#define PB2_GET_MIN _IOR(0x10, 0x35, int32_t *)
+#define PB2_GET_MAX _IOR(0x10, 0x36, int32_t *)
 
 struct obj_info {
     int32_t prio_que_size;  // current number of elements in priority queue
